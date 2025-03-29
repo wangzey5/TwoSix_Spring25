8101948e15a9ab69226fe5bdf92c647e4c1fc6f3
Ethan DeMott <112441099+edemott@users.noreply.github.com>
2025-03-29 17:38:33 -0400

    Remove scrapying.py mongo file and example comments.json

--- mongoUtils.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-def mongoConnect(IPpath="/mnt/research/TwoSix/mongodb/IP.txt"):
-    try:
-        with open(IPpath, "r") as ipFile:
-            ip = ipFile.read()
-            return pymongo.MongoClient(host=[ip])
-    except:
-        return pymongo.MongoClient()
